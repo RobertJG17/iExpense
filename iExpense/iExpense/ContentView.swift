0e5d89cecd1c93845698ffa349e78f1884e43bfa
RobertJG17 <robertjguerra@yahoo.com>
2020-11-04 22:19:52 -0800

    Created Skeleton for Project, added Row Editing, and UUID within ExpenseItem Struct

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,25 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var showingSheet = false
-    
-    var body: some View {
-        Button("Show Sheet") {
-            self.showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView()
-        }
-    }
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
 }
 
-struct SecondView: View {
-    @Environment(\.presentationMode) var presentationMode
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
     
     var body: some View {
-        Button("Dismiss") {
-            self.presentationMode.wrappedValue.dismiss()
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    Text("\(item.name)")
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)
+                }) {
+                Image(systemName: "plus")
+                }
+            )
         }
     }
 }
