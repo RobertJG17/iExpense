308bdb52e74f5d740ea3c13484981b9526acb839
RobertJG17 <robertjguerra@yahoo.com>
2020-11-07 19:06:51 -0800

    Completed Project and Added Error Alert When Users Enter Invalid Value

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct AddView: View {
     @State private var name = ""
     @State private var type = "Personal"
-    @State var amount = ""
+    @State private var showingAlert = false
+    @State var amount: String = ""
     @ObservedObject var expenses: Expenses
+    @Environment(\.presentationMode) var presentationMode
     
     static let types = ["Business", "Personal"]
     
@@ -29,11 +31,17 @@ struct AddView: View {
             }
             .navigationBarTitle("Add new expense")
             .navigationBarItems(trailing: Button("Save") {
-                if let actualAmount = Int(self.amount) {
+                if let actualAmount = Float(self.amount) {
                     let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
                     self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                } else {
+                    showingAlert = true
                 }
             })
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Invalid Input"), message: Text("Please enter a proper value."), dismissButton: .default(Text("OK")))
         }
     }
 }
--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,15 +7,33 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     let id = UUID()
     let name: String
     let type: String
-    let amount: Int
+    let amount: Float
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items: [ExpenseItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        self.items = []
+    }
 }
 
 struct ContentView: View {
@@ -30,12 +48,21 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(expenses.items) { item in
-                    Text("\(item.name)")
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text("$\(item.amount, specifier: "%.2f")")
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
             .navigationBarTitle("iExpense")
-            .navigationBarItems(trailing: Button(action: {
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
                     self.showingAddExpense = true
                 }) {
                 Image(systemName: "plus")
